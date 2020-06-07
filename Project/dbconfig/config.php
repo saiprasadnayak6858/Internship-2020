855bd5668d7443d3c904008045e5ba6e096bf959
Sai Prasad Nayak <saiprasadnayak6858@gmail.com>
2020-06-07 18:19:58 +0530

    testing

--- Project/dbconfig/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","root","") or die("Could not connect");
-    mysqli_select_db($con,"users");
-    $con_class = mysqli_connect("localhost","root","") or die("Could not connect");
-    mysqli_select_db($con_class,"class");
-?>
