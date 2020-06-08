bf2fd5b4f86055651316db741c7c80db6cad3b22
Sai Prasad Nayak <saiprasadnayak6858@gmail.com>
2020-06-08 21:08:41 +0530

    added screenshots folder to assets branch

--- Form/dbconfig/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //Connection for getting users
-    $con = mysqli_connect("localhost","root","") or die("Could not connect");
-    mysqli_select_db($con,"users");
-
-    //Connection for getting students of class
-    $con_class = mysqli_connect("localhost","root","") or die("Could not connect");
-    mysqli_select_db($con_class,"class");
-?>
